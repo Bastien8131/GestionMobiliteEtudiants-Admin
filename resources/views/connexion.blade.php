a81cfc0ce7ae3b53705c8215ab03be537aa1f36e
Ast415 <bastienbories81@gmail.com>
2023-12-14 16:25:39 +0100

    Set main layout pour le header et footer

---
+++ resources/views/connexion.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+    <main>
+        <h1>Connexion</h1>
+    </main>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mon Site</title>
+</head>
+<body>
+@include('includes.header')
+
+@yield('content')
+
+@include('includes.footer')
+</body>
+</html>
