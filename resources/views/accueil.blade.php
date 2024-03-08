168bd400623d08c62101954cb5a2e2d4c3493bbc
Bastien-Meziere <84900801+Bastien-Meziere@users.noreply.github.com>
2024-03-08 14:45:07 +0100

    Ajout lien gestion admin

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.main')
-
+<title>Accueil</title>
 @section('content')
     <main>
         <h1>Accueil</h1>
+        <a href="">Gestion des diplômes</a><br>
+        <a href="">Gestion des programmes</a><br>
+        <a href="">Gestion des cours</a><br>
+        <a href="">Gestion demande mobilité</a><br>
+        <a href="">Gestion des contrats</a><br>
+        <a href="">Gestion demande financement</a><br>
+        <a href="">Gestion des comptes utilisateurs</a>
     </main>
 @endsection
