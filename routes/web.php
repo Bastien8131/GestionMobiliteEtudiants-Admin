35d9c45f71ce1b00367df21aa0b6ea0c1fb80870
Bastien-Meziere <84900801+Bastien-Meziere@users.noreply.github.com>
2024-03-08 14:42:18 +0100

    Changement route par défaut

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('connexion');
+    return view('accueil');
 });
 
-Route::get('/accueil', function () {
-    return view('accueil');
+Route::get('/diplome', function () {
+    return view('diplome');
 });
