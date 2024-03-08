2c1c656161823a556fc869578585c9e4fa292940
Bastien-Meziere <84900801+Bastien-Meziere@users.noreply.github.com>
2024-03-08 14:41:50 +0100

    Suppression fichiers inutiles

--- resources/views/connexion.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main')
-
-@section('content')
-    <main>
-        <h1>Connexion</h1>
-        <a href="./accueil">accueil</a>
-    </main>
-@endsection
--- resources/views/inscription.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.main')
-
-@section('content')
-    <main>
-        <h1>Inscription</h1>
-    </main>
-@endsection
