1dc48b71690416ddac6179a286c3e7fba1ef7ec4
Bastien-Meziere <84900801+Bastien-Meziere@users.noreply.github.com>
2024-03-08 15:09:54 +0100

    Ajout chemin route vue

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <main>
         <h1>Accueil</h1>
-        <a href="">Gestion des diplômes</a><br>
-        <a href="">Gestion des programmes</a><br>
-        <a href="">Gestion des cours</a><br>
-        <a href="">Gestion demande mobilité</a><br>
-        <a href="">Gestion des contrats</a><br>
-        <a href="">Gestion demande financement</a><br>
-        <a href="">Gestion des comptes utilisateurs</a>
+        <a href="{{ url('/diplome') }}">Gestion des diplômes</a><br>
+        <a href="{{ url('/programme') }}">Gestion des programmes</a><br>
+        <a href="{{ url('/cours') }}">Gestion des cours</a><br>
+        <a href="{{ url('/gestionDemandeMobilite') }}">Gestion demande mobilité</a><br>
+        <a href="{{ url('/contrat') }}">Gestion des contrats</a><br>
+        <a href="{{ url('/gestionDemandeFinancement') }}">Gestion demande financement</a><br>
+        <a href="{{ url('/utilisateur') }}">Gestion des comptes utilisateurs</a>
     </main>
 @endsection
