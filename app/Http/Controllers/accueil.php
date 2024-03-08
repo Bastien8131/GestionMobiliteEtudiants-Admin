a9c17e4cf3ace363125afdfd621d1dbf0e7bc173
Bastien-Meziere <84900801+Bastien-Meziere@users.noreply.github.com>
2024-03-08 19:41:21 +0100

    Ajout fonction Controller pour diplome, programme et cours

--- app/Http/Controllers/accueil.php
+++ app/Http/Controllers/accueil.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\diplomes;
+use App\Models\universites;
+use App\Models\programmes;
+use App\Models\cours;
 
 class accueil extends Controller
 {
-    //
+    public function fonctionDiplome()
+    {
+        $diplomes = diplomes::with('universites')->get();
+        $universites = universites::all();
+    
+        return view('diplome', compact('diplomes', 'universites'));
+    }
+
+    public function fonctionProgramme()
+    {
+        $programmes = programmes::with('diplomes')->get();
+        $diplomes = diplomes::all();
+    
+        return view('programme', compact('programmes', 'diplomes'));
+    }
+
+    public function fonctionCours()
+    {
+        $cours = cours::with('diplomes')->get();
+        $diplomes = diplomes::all();
+    
+        return view('cours', compact('cours', 'diplomes'));
+    }
 }
